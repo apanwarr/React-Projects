ca44d9c2e34e9a46e312eb0cd06980fcf69c9119
Ashish Panwar <ashishpanwar413@gmail.com>
2025-03-15 15:26:43 +0530

    React Routers and their uses with diff.  States

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/apanwarr')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github Repository: {data.public_repos}
+    <img className='rounded-3xl p-3 ml-32' src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/apanwarr')
+    return response.json()
+}
